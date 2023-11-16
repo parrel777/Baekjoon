030758e242c6990253dabaf20b3a89fe0ad325d5
KIM DOO HYUN <67574328+parrel777@users.noreply.github.com>
2023-11-16 21:42:32 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int max(int a, int b){
+	return (a > b)? a : b;
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int N;
+	int arr[1001];
+	int dp[1001];
+	int ans = 0;
+	cin >> N;
+	
+	for(int i=0; i<N; i++){
+		cin >> arr[i];
+	}
+	
+	for(int k=0; k<N; k++){
+		dp[k] = 1;
+		for(int i=0; i<k; i++){
+			if(arr[i] > arr[k]){
+				dp[k] = max(dp[k], dp[i] + 1);
+			}
+		}
+		if(ans < dp[k]) ans = dp[k];
+	}
+	
+	cout << ans;
+	
+	return 0;
+}

377b11aef384c15a1ca0a75fa5eb6e884c3d2937
KIM DOO HYUN <67574328+parrel777@users.noreply.github.com>
2023-11-19 15:10:13 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 140 ms, Memory: 10632 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -6,14 +6,6 @@ int N, M;
 int arr[1050][1050];
 int dp[1050][1050];
 
-int solve(int a, int b, int c, int d){
-	int ans;
-	
-	ans = dp[c][d] + dp[a-1][b-1] - (dp[c][b-1] + dp[a-1][d]);
-	
-	return ans;
-}
-
 int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
@@ -21,32 +13,20 @@ int main(){
 	
 	cin >> N >> M;
 	
-	//init
-	for(int i=0; i<=N; i++){
-		for(int j=0; j<=N; j++){
-			if(i==0 || j==0)
-				dp[i][j] = 0;
-		}
-	}
-	
-	// input
+	// input + dp
 	for(int i=1; i<=N; i++){
 		for(int j=1; j<=N; j++){
 			cin >> arr[i][j];
-		}
-	}
-	//dp
-	for(int i=1; i<=N; i++){
-		for(int j=1; j<=N; j++){
 			dp[i][j] = arr[i][j] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1];
 		}
 	}
 	
 	//answer
 	for(int i=0; i<M; i++){
-		int y1, x1, y2, x2;
-		cin >> y1 >> x1 >> y2 >> x2;
-		cout << solve(y1, x1, y2, x2) << '\n';
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+		int ans = dp[c][d] + dp[a-1][b-1] - (dp[c][b-1] + dp[a-1][d]);
+		cout << ans << '\n';
 	}
 	
 	return 0;

8aa60db40250b29050f7721a1fb2000485478861
KIM DOO HYUN <67574328+parrel777@users.noreply.github.com>
2024-03-22 17:17:41 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int n,m;
+int arr[9];
+bool visited[9];
+
+
+void dfs(int cnt)
+{
+    if(cnt == m)
+    {
+        for(int i = 0; i < m; i++){
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(visited[i] == false)
+        {
+        	for(int j=0; j<i; j++)
+            	visited[j] = true;
+            arr[cnt] = i;
+            dfs(cnt+1);
+            for(int j=0; j<i; j++)
+            	visited[j] = false;
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    dfs(0);
+    return 0;
+}
